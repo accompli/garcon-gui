7242617e3a16841acb9bdaeaf8dae170e451743f
Jinhua Than <jinhua@connectholland.nl>
2015-10-28 12:08:39 +0100

    First import of base project

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+ReactDOM.render(
+    <h1>Test test</h1>,
+  document.getElementById('content')
+); 
