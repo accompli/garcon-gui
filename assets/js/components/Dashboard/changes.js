be799af1dcc04d446302fea158d9ad3cd2ac69cb
Jinhua Than <jinhua@connectholland.nl>
2016-01-14 17:23:26 +0100

    Dashboard CSS fixes

--- assets/js/components/Dashboard/changes.js
+++ assets/js/components/Dashboard/changes.js
@@ -16,12 +16,28 @@ var Changes = React.createClass({
     showStatus: function(){
         if (this.props.row.status === "Error"){
             return (
-                    <div className="status error">{this.props.row.status}</div>
+                    <div className="status error">
+                    <span>
+                    <svg className="error" fill="#000000" height="20" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
+                    </svg>
+                    </span>
+            <span>
+                    {this.props.row.status}
+                    </span></div>
                     );
             }
         else {
             return (
-                    <div className="status available">{this.props.row.status}</div>
+                    <div className="status available">
+                    
+                    <svg className="available" fill="#000000" height="20" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
+                    </svg>
+            
+                    {this.props.row.status}</div>
                     );
             }
     },
--- assets/jsx-output/components/Dashboard/changes.js
+++ assets/jsx-output/components/Dashboard/changes.js
@@ -31,12 +31,32 @@ var Changes = _react2['default'].createClass({
             return _react2['default'].createElement(
                 'div',
                 { className: 'status error' },
-                this.props.row.status
+                _react2['default'].createElement(
+                    'span',
+                    null,
+                    _react2['default'].createElement(
+                        'svg',
+                        { className: 'error', fill: '#000000', height: '20', viewBox: '0 0 24 24', width: '24', xmlns: 'http://www.w3.org/2000/svg' },
+                        _react2['default'].createElement('path', { d: 'M0 0h24v24H0z', fill: 'none' }),
+                        _react2['default'].createElement('path', { d: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z' })
+                    )
+                ),
+                _react2['default'].createElement(
+                    'span',
+                    null,
+                    this.props.row.status
+                )
             );
         } else {
             return _react2['default'].createElement(
                 'div',
                 { className: 'status available' },
+                _react2['default'].createElement(
+                    'svg',
+                    { className: 'available', fill: '#000000', height: '20', viewBox: '0 0 24 24', width: '24', xmlns: 'http://www.w3.org/2000/svg' },
+                    _react2['default'].createElement('path', { d: 'M0 0h24v24H0z', fill: 'none' }),
+                    _react2['default'].createElement('path', { d: 'M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z' })
+                ),
                 this.props.row.status
             );
         }
